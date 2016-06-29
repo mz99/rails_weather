35558297584b5c45b40778ad60c3c85dcdf5d237
Mike Zhang <ubik00@yahoo.com>
2016-06-29 16:49:22 +0200

    successfully fetched 3 attributes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,12 +2,12 @@ class WelcomeController < ApplicationController
 
   def weather
     require 'open-uri'
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/IA/Cedar_Rapids.json') do |f|
+    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/Berlin.json') do |f|
       json_string = f.read
       parsed_json = JSON.parse(json_string)
-      @location = parsed_json['location']['city']
-      temp_f = parsed_json['current_observation']['temp_f']
-      print "Current temperature in #{location} is: #{temp_f}\n"
+      @full_location = parsed_json['current_observation']['display_location']['full']
+      @current_temp = parsed_json['current_observation']['temp_f']
+      @feels_like = parsed_json['current_observation']['feelslike_f']
     end
   end
 end
