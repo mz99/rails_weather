c0d98509747485a4b140dab26fa4b22196f8c0ea
Mike Zhang <ubik00@yahoo.com>
2016-06-29 16:13:11 +0200

    added require 'open-uri' to make api call work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 
   def weather
+    require 'open-uri'
     open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/IA/Cedar_Rapids.json') do |f|
       json_string = f.read
       parsed_json = JSON.parse(json_string)
@@ -8,5 +9,5 @@ class WelcomeController < ApplicationController
       temp_f = parsed_json['current_observation']['temp_f']
       print "Current temperature in #{location} is: #{temp_f}\n"
     end
-
   end
+end
