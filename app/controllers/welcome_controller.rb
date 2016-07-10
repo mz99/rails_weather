480cc03314bd3aa25d5bba9ae7a41c043ae22e96
Mike Zhang <ubik00@yahoo.com>
2016-07-10 17:32:47 +0200

    added images and css to german cities

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,9 +5,27 @@ class WelcomeController < ApplicationController
     open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/Berlin.json') do |f|
       json_string = f.read
       parsed_json = JSON.parse(json_string)
-      @full_location = parsed_json['current_observation']['display_location']['full']
-      @current_temp = parsed_json['current_observation']['temp_f']
-      @feels_like = parsed_json['current_observation']['feelslike_f']
+      @berlin_full_location = parsed_json['current_observation']['display_location']['full']
+      @berlin_current_temp = parsed_json['current_observation']['temp_f']
+      @berlin_feels_like = parsed_json['current_observation']['feelslike_f']
     end
+
+    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/Munich.json') do |f|
+      json_string = f.read
+      parsed_json = JSON.parse(json_string)
+      @munich_full_location = parsed_json['current_observation']['display_location']['full']
+      @munich_current_temp = parsed_json['current_observation']['temp_f']
+      @munich_feels_like = parsed_json['current_observation']['feelslike_f']
+    end
+
+    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/dortmund.json') do |f|
+      json_string = f.read
+      parsed_json = JSON.parse(json_string)
+      @dortmund_full_location = parsed_json['current_observation']['display_location']['full']
+      @dortmund_current_temp = parsed_json['current_observation']['temp_f']
+      @dortmund_feels_like = parsed_json['current_observation']['feelslike_f']
+    end
+
   end
+
 end
