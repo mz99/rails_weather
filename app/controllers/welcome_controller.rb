260e1aabbee8def48cd4e9101d00486972bccb16
Mike Zhang <ubik00@yahoo.com>
2016-07-10 17:52:25 +0200

    added images and css to mediterranean places

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -26,6 +26,30 @@ class WelcomeController < ApplicationController
       @dortmund_feels_like = parsed_json['current_observation']['feelslike_f']
     end
 
+    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Italy/rome.json') do |f|
+      json_string = f.read
+      parsed_json = JSON.parse(json_string)
+      @rome_full_location = parsed_json['current_observation']['display_location']['full']
+      @rome_current_temp = parsed_json['current_observation']['temp_f']
+      @rome_feels_like = parsed_json['current_observation']['feelslike_f']
+    end
+
+    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Greece/mykonos.json') do |f|
+      json_string = f.read
+      parsed_json = JSON.parse(json_string)
+      @mykonos_full_location = parsed_json['current_observation']['display_location']['full']
+      @mykonos_current_temp = parsed_json['current_observation']['temp_f']
+      @mykonos_feels_like = parsed_json['current_observation']['feelslike_f']
+    end
+
+    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Spain/barcelona.json') do |f|
+      json_string = f.read
+      parsed_json = JSON.parse(json_string)
+      @barcelona_full_location = parsed_json['current_observation']['display_location']['full']
+      @barcelona_current_temp = parsed_json['current_observation']['temp_f']
+      @barcelona_feels_like = parsed_json['current_observation']['feelslike_f']
+    end
+
   end
 
 end
