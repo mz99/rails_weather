09fa811056c2ee09f08b434f4b7a4051d1936633
Mike Zhang <ubik00@yahoo.com>
2016-07-11 20:52:31 +0200

    refactored api call to use private method for cleaner code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,55 +1,46 @@
 class WelcomeController < ApplicationController
 
+#index call
   def weather
     require 'open-uri'
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/Berlin.json') do |f|
-      json_string = f.read
-      parsed_json = JSON.parse(json_string)
-      @berlin_full_location = parsed_json['current_observation']['display_location']['full']
-      @berlin_current_temp = parsed_json['current_observation']['temp_f']
-      @berlin_feels_like = parsed_json['current_observation']['feelslike_f']
-    end
+    parsed_json = get_weather_data("Germany/Berlin")
+    @berlin_full_location = parsed_json['current_observation']['display_location']['full']
+    @berlin_current_temp = parsed_json['current_observation']['temp_f']
+    @berlin_feels_like = parsed_json['current_observation']['feelslike_f']
 
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/Munich.json') do |f|
-      json_string = f.read
-      parsed_json = JSON.parse(json_string)
-      @munich_full_location = parsed_json['current_observation']['display_location']['full']
-      @munich_current_temp = parsed_json['current_observation']['temp_f']
-      @munich_feels_like = parsed_json['current_observation']['feelslike_f']
-    end
+    parsed_json = get_weather_data("Germany/Munich")
+    @munich_full_location = parsed_json['current_observation']['display_location']['full']
+    @munich_current_temp = parsed_json['current_observation']['temp_f']
+    @munich_feels_like = parsed_json['current_observation']['feelslike_f']
 
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Germany/dortmund.json') do |f|
-      json_string = f.read
-      parsed_json = JSON.parse(json_string)
-      @dortmund_full_location = parsed_json['current_observation']['display_location']['full']
-      @dortmund_current_temp = parsed_json['current_observation']['temp_f']
-      @dortmund_feels_like = parsed_json['current_observation']['feelslike_f']
-    end
+    parsed_json = get_weather_data("Germany/Dortmund")
+    @dortmund_full_location = parsed_json['current_observation']['display_location']['full']
+    @dortmund_current_temp = parsed_json['current_observation']['temp_f']
+    @dortmund_feels_like = parsed_json['current_observation']['feelslike_f']
 
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Italy/rome.json') do |f|
-      json_string = f.read
-      parsed_json = JSON.parse(json_string)
-      @rome_full_location = parsed_json['current_observation']['display_location']['full']
-      @rome_current_temp = parsed_json['current_observation']['temp_f']
-      @rome_feels_like = parsed_json['current_observation']['feelslike_f']
-    end
+    parsed_json = get_weather_data("Italy/Rome")
+    @rome_full_location = parsed_json['current_observation']['display_location']['full']
+    @rome_current_temp = parsed_json['current_observation']['temp_f']
+    @rome_feels_like = parsed_json['current_observation']['feelslike_f']
 
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Greece/mykonos.json') do |f|
-      json_string = f.read
-      parsed_json = JSON.parse(json_string)
-      @mykonos_full_location = parsed_json['current_observation']['display_location']['full']
-      @mykonos_current_temp = parsed_json['current_observation']['temp_f']
-      @mykonos_feels_like = parsed_json['current_observation']['feelslike_f']
-    end
+    parsed_json = get_weather_data("Greece/Mykonos")
+    @mykonos_full_location = parsed_json['current_observation']['display_location']['full']
+    @mykonos_current_temp = parsed_json['current_observation']['temp_f']
+    @mykonos_feels_like = parsed_json['current_observation']['feelslike_f']
 
-    open('http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/Spain/barcelona.json') do |f|
-      json_string = f.read
-      parsed_json = JSON.parse(json_string)
-      @barcelona_full_location = parsed_json['current_observation']['display_location']['full']
-      @barcelona_current_temp = parsed_json['current_observation']['temp_f']
-      @barcelona_feels_like = parsed_json['current_observation']['feelslike_f']
-    end
+    parsed_json = get_weather_data("Spain/Barcelona")
+    @barcelona_full_location = parsed_json['current_observation']['display_location']['full']
+    @barcelona_current_temp = parsed_json['current_observation']['temp_f']
+    @barcelona_feels_like = parsed_json['current_observation']['feelslike_f']
+  end
 
+  private
+
+  def get_weather_data(location)
+    open("http://api.wunderground.com/api/b8c9cc80d5440996/geolookup/conditions/q/#{location}.json") do |f|
+      json_string = f.read
+      JSON.parse(json_string)
+    end
   end
 
 end
